f6f4bce8392b49d28425161b971900b47f2fe502
Maxim Klimenko <maximklimenko4@gmail.com>
2017-03-30 01:05:15 +0300

    creating submit

--- MaximKlimenko/src/main/test/java/ua/kiev/prog/week1/SubmitFormTest.java
+++ MaximKlimenko/src/main/test/java/ua/kiev/prog/week1/SubmitFormTest.java
@@ -1,24 +1,46 @@
-package java.ua.kiev.prog.week1;
-
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+package ua.kiev.prog.week1;
+import org.junit.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class SubmitFormTest {
 
     static WebDriver driver;
+    static WebDriverWait wait;
 
 
     @BeforeClass
     public static void beforeAll() {
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "D:\\projects\\AQA\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, 4);
+
     }
 
     @Test
-    public void name() throws Exception {
+    public void submitForm() {
+        driver.get("http://derp-bear.herokuapp.com/forms/basic_form_example");
+        driver.findElement(By.id("first_name")).sendKeys("TestName");
+        driver.findElement(By.id("last_name")).sendKeys("Test Lastname");
+        driver.findElement(By.id("email")).sendKeys("mail@mail.com");
+        driver.findElement(By.id("website_url")).sendKeys("http://www.google.com.ua");
+        driver.findElement(By.cssSelector(".btn-group.select.btn-md.btn-primary")).click();
+        WebElement dropdownMenu = wait.until(ExpectedConditions.visibilityOfElementLocated(By
+                .cssSelector(".btn-primary.open ul li:nth-child(2)")));
+        dropdownMenu.click();
+
+    }
+
+    @After
+    public void tearDown() {
+//        driver.quit();
 
     }
 }
