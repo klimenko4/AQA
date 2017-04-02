7a90746060e1195dea26b0cb676cfaf040a77d99
Maxim Klimenko <maximklimenko4@gmail.com>
2017-04-03 00:43:17 +0300

    Parking app done

--- MaximKlimenko/src/main/java/ua/kiev/prog/week1/parkingapp/MotorCycle.java
+++ MaximKlimenko/src/main/java/ua/kiev/prog/week1/parkingapp/MotorCycle.java
@@ -1,38 +1,11 @@
 package ua.kiev.prog.week1.parkingapp;
 
-/**
- * Created by Maxim Klimenko on 27.03.2017.
- */
-public class MotorCycle {
+public class MotorCycle extends Vehicle {
 
-    private String plateNumber;
-    private String model;
-    private String ownerName;
     public MotorCycle(String plateNumber,String model,String ownerName) {
-        this.plateNumber = plateNumber;
-        this.model = model;
-        this.ownerName = ownerName;
+        setPlateNumber(plateNumber);
+        setModel(model);
+        setOwnerName(ownerName);
     }
 
-    public void setPlateNumber(String plateNumber) {
-        this.plateNumber = plateNumber;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public void setOwnerName(String ownerName) {
-        this.ownerName = ownerName;
-    }
-
-    public String getPlateNumber() {
-        return plateNumber;
-    }
-
-    public String getModel() {return model; }
-
-    public String getOwnerName() {
-        return ownerName;
-    }
 }
--- MaximKlimenko/src/main/java/ua/kiev/prog/week1/parkingapp/ParkingAppication.java
+++ MaximKlimenko/src/main/java/ua/kiev/prog/week1/parkingapp/ParkingAppication.java
@@ -5,9 +5,38 @@ package ua.kiev.prog.week1.parkingapp;
  */
 public class ParkingAppication {
     public static void main(String[] args) {
-        MotorCycle cycle1 = new MotorCycle("4422", "bugatti", "Peter Blood");
+        MotorCycle cycle1 = new MotorCycle("4422", "bugatti", "Peter Blood1");
+        MotorCycle cycle2 = new MotorCycle("4422", "bugatti", "Peter Blood2");
+        MotorCycle cycle3 = new MotorCycle("4422", "bugatti", "Peter Blood3");
 
         Parking parking = new Parking();
+//        parking.status();
+        parking.open();
+//        parking.status();
+        parking.addVehicleAtFreePlace(cycle1);
+        parking.addVehicleAtFreePlace(cycle2);
+        parking.addVehicleAtFreePlace(cycle3);
+//        parking.addVehicleAtFreePlace(cycle1);
+//        parking.addVehicleAtFreePlace(cycle1);
+
+//        parking.showVehicleByPlace();
+//        parking.showParking();
+//        parking.removeVehicleFromLastPlace();
+//        System.out.println("");
+//        parking.showParking();
+
+//        parking.addVehicleByPlace(cycle2,9);
+//        parking.showVehicleByPlace(9);
+//        parking.removeVehicleByPlace(9);
+//        parking.removeVehicleByPlace(9);
+
+//        parking.takeLastVehicle();
+        parking.changeAddress(1,2);
+        parking.showParking();
+        parking.close();
+        parking.clearParking();
+        System.out.println("");
+        parking.showParking();
 
     }
 
