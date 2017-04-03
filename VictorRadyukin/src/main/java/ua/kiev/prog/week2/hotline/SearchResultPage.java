dd32be21e02b4db29784b72071bcc274ee1561fc
radyukin <radyukin@gmail.com>
2017-04-04 00:29:32 +0300

    Add enum structure, less hardcoding etc

--- VictorRadyukin/src/main/java/ua/kiev/prog/week2/hotline/SearchResultPage.java
+++ VictorRadyukin/src/main/java/ua/kiev/prog/week2/hotline/SearchResultPage.java
@@ -8,30 +8,109 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 /**
  * Created by Zver on 30.03.2017.
  */
 public class SearchResultPage {
+
     private WebDriver driver;
 
+    private WebDriverWait wait;
+
+    //Set for storing all applied PriceRanges
+    private Set<PriceRange> priceRangeList = new TreeSet<PriceRange>();
+
     //bottom blue toolbar, which hides price filters
-    @FindBy (xpath = "//*[@id=\"mm-0\"]/div[3]/div/div[1]/a[3]")
+    @FindBy(xpath = "//*[@id=\"mm-0\"]/div[3]/div/div[1]/a[3]")
     private WebElement bottomToolbar;
 
-    @FindBy (partialLinkText = "2500-3500")
-    private WebElement checkbox_1;
+    //@FindBy (partialLinkText = "2500-3500")
+    private WebElement checkbox;
 
-    @FindBy (xpath = "//a[@href='/mobile/mobilnye-telefony-i-smartfony/294245-11857-11858/']")
-    private WebElement checkbox_2;
-
-    public SearchResultPage(WebDriver driver) {
+    SearchResultPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, 20);
         PageFactory.initElements(driver, this);
     }
 
+    private Set<PriceRange> getPriceRangeList() {
+        return priceRangeList;
+    }
+
+    //return min price from priceRangeList
+    public int getMinPriceFromPriceRangeList() throws Exception {
+        if (this.priceRangeList.isEmpty()) {
+            throw new Exception("Filter's list is empty, add one ore more PriceRanges");
+        }
+        Iterator<PriceRange> iterator = this.getPriceRangeList().iterator();
+        int minPrice = iterator.next().getMinPrice();
+        while(iterator.hasNext()) {
+            PriceRange priceRange = iterator.next();
+            int minPriceFromPriceRange = priceRange.getMinPrice();
+            if (minPriceFromPriceRange < minPrice) {
+                minPrice = minPriceFromPriceRange;
+            }
+        }
+        return minPrice;
+    }
+
+    //return min price from priceRangeList
+    public int getMaxPriceFromPriceRangeList() throws Exception {
+        if (this.priceRangeList.isEmpty()) {
+            throw new Exception("Filter's list is empty, add one ore more PriceRanges");
+        }
+        Iterator<PriceRange> iterator = this.getPriceRangeList().iterator();
+        int maxPrice = iterator.next().getMaxPrice();
+        while(iterator.hasNext()) {
+            PriceRange priceRange = iterator.next();
+            int maxPriceFromPriceRange = priceRange.getMaxPrice();
+            if (maxPriceFromPriceRange > maxPrice) {
+                maxPrice = maxPriceFromPriceRange;
+            }
+        }
+        return maxPrice;
+    }
+
+    //hide bottom toolbar
+    public SearchResultPage hideBottomToolbar() {
+        //closing bottom blue toolbar, which hiding filters
+        wait.until(ExpectedConditions.elementToBeClickable(bottomToolbar));
+        bottomToolbar.click();
+        return this;
+    }
+
+    //select priceRange from enum PriceRange obj, add PriceRange obj to priceRangeList
+    public SearchResultPage selectPriceFilter(PriceRange priceRange) {
+        //adding enum obj to total set of all applied price filters
+        priceRangeList.add(priceRange);
+        //choose price range checkbox
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        checkbox = driver.findElement(By.partialLinkText(priceRange.getRangeString()));
+        wait.until(ExpectedConditions.elementToBeClickable(checkbox));
+        checkbox.click();
+        return this;
+    }
+
+    //get all item's prices after applying priceRange filters
+    public List<Integer> getAllPricesInRange() {
+        List<Integer> pricesIntegers = new ArrayList<Integer>();
+        List<WebElement> pricesBlocks = driver.findElements(By.xpath("//div[@class='text-14 text-13-480 orng']/b"));
+        for (WebElement priceBlock: pricesBlocks) {
+            String price = priceBlock.getText();
+            int len = price.length();
+            pricesIntegers.add(Integer.parseInt(removeWhitespacesFromStr(price.substring(0, len - 3))));
+        }
+        return pricesIntegers;
+    }
+
+    //remove whitespaces
     private String removeWhitespacesFromStr (String inputStr) {
         String resultString = "";
         for(int i = 0; i < inputStr.length(); i++) {
@@ -40,34 +119,5 @@ public class SearchResultPage {
             }
         }
         return resultString;
-    }
-    //choosing first two price filters
-    public SearchResultPage selectFilters() {
-        WebDriverWait wait = new WebDriverWait(driver, 20);
-        //closing bottom blue toolbar, which hiding filters
-        bottomToolbar.click();
-        //choose price filter 2500-3500
-        wait.until(ExpectedConditions.elementToBeClickable(checkbox_1));
-        checkbox_1.click();
-        //choose price filter 3500-5000
-        wait.until(ExpectedConditions.elementToBeClickable(checkbox_2));
-        checkbox_2.click();
-        return this;
-    }
-
-    public void checkPricesInRange() {
-        List<Integer> pricesIntegers = new ArrayList<Integer>();
-        List<WebElement> pricesBlocks = driver.findElements(By.xpath("//div[@class='text-14 text-13-480 orng']/b"));
-        for (WebElement priceBlock: pricesBlocks) {
-            String price = priceBlock.getText();
-            int len = price.length();
-            pricesIntegers.add(Integer.parseInt(removeWhitespacesFromStr(price.substring(0, len - 3))));
-        }
-        for(int price: pricesIntegers) {
-            System.out.println(price);
-        }
-        for(int price: pricesIntegers) {
-            Assert.assertTrue("Item's price is out of range", (price >= 2500 && price <= 5000));
-        }
     }
 }
