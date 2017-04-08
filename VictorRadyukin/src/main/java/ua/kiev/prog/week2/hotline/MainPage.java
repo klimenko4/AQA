7236cc1528243900c6643f9a2b3a508039a073c7
radyukin <radyukin@gmail.com>
2017-04-08 23:46:44 +0300

    add SignInUserTest

--- VictorRadyukin/src/main/java/ua/kiev/prog/week2/hotline/MainPage.java
+++ VictorRadyukin/src/main/java/ua/kiev/prog/week2/hotline/MainPage.java
@@ -1,8 +1,6 @@
 package ua.kiev.prog.week2.hotline;
 
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,6 +21,18 @@ public class MainPage extends BasePage {
     @FindBy(css = "a.reg")
     private WebElement registerLink;
 
+    @FindBy(xpath = "//a[@href = '/user/log_in/']")
+    private WebElement signInLink;
+
+    @FindBy(xpath = "//input[@name = 'login']")
+    private WebElement loginInputField;
+
+    @FindBy(xpath = "//input[@name = 'password']")
+    private WebElement passwordInputField;
+
+    @FindBy(xpath = "//input[@value = 'Войти']")
+    private WebElement signInButton;
+
     public MainPage(WebDriver driver, WebDriverWait wait) {
         super(driver, wait);
         PageFactory.initElements(driver, this);
@@ -31,6 +41,7 @@ public class MainPage extends BasePage {
     //put url to driver
     public MainPage open() {
         driver.get(url);
+        driver.manage().window().maximize();
         return this;
     }
 
@@ -44,5 +55,35 @@ public class MainPage extends BasePage {
         wait.until(ExpectedConditions.elementToBeClickable(registerLink));
         registerLink.click();
         return new RegisterNewUserPage(driver, wait);
+    }
+
+    public MainPage openSignInPopupWindow() {
+        wait.until(ExpectedConditions.elementToBeClickable(signInLink));
+        signInLink.click();
+        return this;
+    }
+
+    public MainPage fillAndSubmitSignInForm(String email, String password) {
+        if (driver instanceof JavascriptExecutor) {
+            ((JavascriptExecutor) driver)
+                    .executeScript("var closeLink = document.querySelector('a.close-x'); console.log(closeLink);" +
+                            "closeLink.click();");
+        }
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.box_user_log")));
+        loginInputField.sendKeys(email);
+        passwordInputField.sendKeys(password);
+        signInButton.click();
+        return this;
+    }
+
+    public Boolean isSuccessedUserSignIn(String usernameRegisteredUser) {
+        String xpathUsernameElement = "//span[@title = '" + usernameRegisteredUser + "']";
+        //return (driver.findElement(By.xpath(xpathUsernameElement)).isDisplayed());
+        try {
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpathUsernameElement)));
+        } catch (RuntimeException exception) {
+            return false;
+        }
+        return true;
     }
 }
